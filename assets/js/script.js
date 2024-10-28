91d4430189eae0e0acc1178abbf5fe2bc45bf994
rgsnabrhm77 <rqsanibrahimli@gmail.com>
2024-10-28 19:15:33 +0400

    project

--- assets/js/script.js
+++ assets/js/script.js
@@ -336,54 +336,30 @@ document.addEventListener('mousemove', function (e) {
 });
 
 
-document.addEventListener('mousemove', function (e) {
-    const x = e.pageX;
-    const y = e.pageY;
 
-    const gradientOverlay = document.getElementById('gradient-overlay');
 
-    gradientOverlay.style.background = ` radial-gradient(600px at ${x}px ${y}px, rgba(29, 78, 216, 0.15), transparent 80%)`;
+
+const menuItems = document.querySelectorAll('.menu-item');
+const cont = document.querySelectorAll('.content');
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        menuItems.forEach(i => i.classList.remove('active'));
+        item.classList.add('active');
+
+        const selectedMenu = item.getAttribute('data-menu');
+        cont.forEach(content => {
+            if (content.id === selectedMenu) {
+                content.classList.add('active');
+            } else {
+                content.classList.remove('active');
+            }
+        });
+    });
 });
 
+document.querySelector('.menu-item[data-menu="about"]').classList.add('active');
+document.querySelector('#about').classList.add('active');
 
 
-
-// const menuItems = document.querySelectorAll('.menu-item');
-// const cont = document.querySelectorAll('.content');
-
-// menuItems.forEach(item => {
-//     item.addEventListener('click', () => {
-//         menuItems.forEach(i => i.classList.remove('active'));
-//         item.classList.add('active');
-
-//         const selectedMenu = item.getAttribute('data-menu');
-//         cont.forEach(content => {
-//             if (content.id === selectedMenu) {
-//                 content.classList.add('active');
-//             } else {
-//                 content.classList.remove('active');
-//             }
-//         });
-//     });
-// });
-
-// document.querySelector('.menu-item[data-menu="about"]').classList.add('active');
-// document.querySelector('#about').classList.add('active');
-
-// projets archive
-
-const tBody = document.getElementById('tbody')
-
-function archiveProjects(){
-    tBody.innerHTML = projects.map(product =>`
-        <tr>
-                                <td>${product.year}</td>
-                                <td>${product.project}</td>
-                                <td>${product.madeAt}</td>
-                                <td>${product.builtWith}</td>
-                                <td><a href="${product.link}" target="_blank" alt="${product.name}">emersoncollective.com</a>
-                                </td>
-                            </tr>
-        `).join('')
-}
 
